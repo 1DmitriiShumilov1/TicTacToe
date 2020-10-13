3d9c09743a879025091b7402d8bf83aad0188b86
1DmitriiShumilov1 <dimashumilov32@gmail.com>
2020-10-13 09:27:16 -0700

    implemented simple AI

--- script.js
+++ script.js
@@ -20,6 +20,17 @@ function game(){
         }
     }
 
+    function pickRandom(){
+        var emptyCells = []
+        for (i = 0; i < cells.length; i++){
+            if (cells[i].innerText == ''){
+                emptyCells.push(cells[i])
+            }
+        }
+        var emptyCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        emptyCell.innerText = 'o'
+    }
+
     function checkForWinOrLoss(){
         if (cells[0].innerText == 'x'){
             if (cells[1].innerText == 'x'){
@@ -168,14 +179,62 @@ function game(){
     }
 
     function computerTurn(){
-        var emptyCells = []
-        for (i = 0; i< cells.length; i++){
-            if (cells[i].innerText == ''){
-                emptyCells.push(cells[i])
-            }
+        var possibleWins = [
+            [cells[0], cells[1], cells[2]], //horizontal 1
+            [cells[3], cells[4], cells[5]], //horizontal 2
+            [cells[6], cells[7], cells[8]], //horizontal 3
+
+            [cells[0], cells[3], cells[6]], //vertical 1
+            [cells[1], cells[4], cells[7]], //vertical 2
+            [cells[2], cells[5], cells[8]], //vertical 3
+
+            [cells[0], cells[4], cells[8]], //diagonal 1
+            [cells[6], cells[4], cells[2]]  //diagonal 2
+        ]
+        if (movesMade == 1){
+            pickRandom()
         }
-        var item = emptyCells[Math.floor(Math.random() * emptyCells.length)]
-        item.innerText = 'o'
+        
+        if (movesMade > 1){
+            for (i = 0; i < possibleWins.length; i++){
+                //check if there's a chance for win//
+                if (((possibleWins[i][0].innerText == 'o') && (possibleWins[i][2].innerText == 'o')) && (possibleWins[i][1].innerText == '')){
+                    console.log(possibleWins[i][0], possibleWins[i][2], 'win')
+                    possibleWins[i][1].innerText = 'o'
+                    return
+                }
+                if (((possibleWins[i][0].innerText == 'o') && (possibleWins[i][1].innerText == 'o')) && (possibleWins[i][2].innerText == '')){
+                    console.log(possibleWins[i][0], possibleWins[i][1], 'win')
+                    possibleWins[i][2].innerText = 'o'
+                    return
+                }
+                if (((possibleWins[i][1].innerText == 'o') && (possibleWins[i][2].innerText == 'o')) && (possibleWins[i][0].innerText == '')){
+                    console.log(possibleWins[i][1], possibleWins[i][2], 'win')
+                    possibleWins[i][0].innerText = 'o'
+                    return
+                }
+
+                // check if need to defend //
+                if (((possibleWins[i][0].innerText == 'x') && (possibleWins[i][2].innerText == 'x')) && (possibleWins[i][1].innerText == '')){
+                    console.log(possibleWins[i][0], possibleWins[i][2], 'defend')
+                    possibleWins[i][1].innerText = 'o'
+                    return
+                }
+                if (((possibleWins[i][0].innerText == 'x') && (possibleWins[i][1].innerText == 'x')) && (possibleWins[i][2].innerText == '')){
+                    console.log(possibleWins[i][0], possibleWins[i][1], 'defend')
+                    possibleWins[i][2].innerText = 'o'
+                    return
+                }
+                if (((possibleWins[i][1].innerText == 'x') && (possibleWins[i][2].innerText == 'x')) && (possibleWins[i][0].innerText == '')){
+                    console.log(possibleWins[i][1], possibleWins[i][2], 'defend')
+                    possibleWins[i][0].innerText = 'o'
+                    return
+                }
+            }
+            // if there's no chance to win or defend //
+            pickRandom()
+            return
+        }
     }
     
     function cellClicked(event){
